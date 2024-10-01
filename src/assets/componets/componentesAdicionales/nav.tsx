07c588acfa8ccd80bfbfdff0f9ac79e9c1d973e3
santiago hernandez <elsantihg@gmail.com>
2024-10-01 11:57:47 -0500

    fix: Creacion de boton de salir

--- src/assets/componets/componentesAdicionales/nav.tsx
+++ src/assets/componets/componentesAdicionales/nav.tsx
@@ -1,7 +1,7 @@
 import {
   Disclosure,
   DisclosurePanel,
-  Menu,
+  
 } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Link, useLocation } from 'react-router-dom';
